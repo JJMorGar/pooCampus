099b744e7b1d218a94b380a30c930f0b6570758e
JJMorGar <97064514+JJMorGar@users.noreply.github.com>
2023-05-26 22:36:28 +0000

    feat(funcionalidad): :sparkles: add module Persona

--- js/Persona.js
+++ js/Persona.js
@@ -1,4 +1,8 @@
 class Persona {
+  static esMayorDeEdad(edad) {
+    return edad >= 18
+  }
+
   #nombre;
   #edad;
   #sexo;
@@ -47,20 +51,33 @@ class Estudiante extends Persona {
   }
 }
 
+const estudiante1 = new Estudiante({ nombre: "carlos", edad: 34, sexo: "a", carrera: "si" })
+console.log(Persona.esMayorDeEdad(estudiante1.edad));
+
 
 const formPersona = document.querySelector('#form-persona');
+const formEstudiante = document.querySelector('#form-estudiante')
+const citaPersona = document.querySelector('#cita-persona')
+const citaEstudiante = document.querySelector('#cita-estudiante')
 
 const cargarPersona = (e) => {
   e.preventDefault()
-  const dataPersona = Object.fromEntries(new FormData(formPersona))  
+  const dataPersona = Object.fromEntries(new FormData(formPersona))
   const persona = new Persona(dataPersona)
+  citaPersona.innerHTML = ""
+  citaPersona.append(persona.saludar())
+}
+const cargarEstudiane = (e) => {
+  e.preventDefault()
+  const dataEstudiante = Object.fromEntries(new FormData(formEstudiante))
+  const estudiante = new Estudiante(dataEstudiante)
+  const button = e.submitter
+  citaEstudiante.innerHTML = ""
+  citaEstudiante.append(button.id === "estudiar" ? estudiante.estudiar() : estudiante.saludar())
 }
 
-
-
-
-formPersona.addEventListener('submit', cargarPersona )
-
+formPersona.addEventListener('submit', cargarPersona)
+formEstudiante.addEventListener('submit', cargarEstudiane)
 
 
 
